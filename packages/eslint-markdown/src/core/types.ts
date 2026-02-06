0e59a250f5f2499bc297a028f9d0d8e54af400ef
루밀LuMir <rpfos@naver.com>
2026-02-06 23:00:14 +0900

    wip: create `Nullable` type

--- packages/eslint-markdown/src/core/types.ts
+++ packages/eslint-markdown/src/core/types.ts
@@ -15,6 +15,11 @@ import type {
 // Typedef
 // --------------------------------------------------------------------------------
 
+/**
+ * Represents a rule module with specific rule options and message IDs.
+ * @template RuleOptions The rule options.
+ * @template MessageIds The message IDs.
+ */
 export type RuleModule<
   RuleOptions extends MarkdownRuleDefinitionTypeOptions['RuleOptions'],
   MessageIds extends MarkdownRuleDefinitionTypeOptions['MessageIds'],
@@ -28,3 +33,9 @@ export type RuleModule<
     stylistic: boolean;
   }>;
 }>;
+
+/**
+ * Represents a type that can either be of type `T` or `null`.
+ * @template T The base type.
+ */
+export type Nullable<T> = T | null;
