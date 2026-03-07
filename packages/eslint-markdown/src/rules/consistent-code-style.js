cb66ddbc2d9c89e6f391387667af55a213de0950
루밀LuMir <rpfos@naver.com>
2026-03-07 21:33:52 +0900

    wip

--- packages/eslint-markdown/src/rules/consistent-code-style.js
+++ packages/eslint-markdown/src/rules/consistent-code-style.js
@@ -17,6 +17,7 @@
 // Import
 // --------------------------------------------------------------------------------
 
+import { isBlankLine } from '../core/ast/index.js';
 import { URL_RULE_DOCS } from '../core/constants.js';
 
 // --------------------------------------------------------------------------------
@@ -26,8 +27,8 @@ import { URL_RULE_DOCS } from '../core/constants.js';
 /**
  * @import { RuleModule } from '../core/types.js';
  * @typedef {'indent' | 'fence-backtick' | 'fence-tilde'} CodeStyle
- * @typedef {[{ style: 'consistent' | CodeStyle, blankLineAbove: number, blankLineBelow: number }]} RuleOptions
- * @typedef {'style'} MessageIds
+ * @typedef {[{ style: 'consistent' | CodeStyle, blankLineAbove: number | false, blankLineBelow: number | false }]} RuleOptions
+ * @typedef {'style' | 'blankLineAbove' | 'blankLineBelow'} MessageIds
  */
 
 // --------------------------------------------------------------------------------
@@ -76,6 +77,28 @@ export default {
           style: {
             enum: ['consistent', 'indent', 'fence-backtick', 'fence-tilde'],
           },
+          blankLineAbove: {
+            oneOf: [
+              {
+                enum: [false],
+              },
+              {
+                type: 'integer',
+                minimum: 1,
+              },
+            ],
+          },
+          blankLineBelow: {
+            oneOf: [
+              {
+                enum: [false],
+              },
+              {
+                type: 'integer',
+                minimum: 1,
+              },
+            ],
+          },
         },
         additionalProperties: false,
       },
@@ -84,13 +107,17 @@ export default {
     defaultOptions: [
       {
         style: 'consistent',
-        blankLineAbove: 1,
-        blankLineBelow: 1,
+        blankLineAbove: false,
+        blankLineBelow: false,
       },
     ],
 
     messages: {
       style: 'Code style should be `{{ style }}`.',
+      blankLineAbove:
+        'Code should be surrounded by {{ blankLineAbove }} blank line(s) above.',
+      blankLineBelow:
+        'Code should be surrounded by {{ blankLineBelow }} blank line(s) below.',
     },
 
     language: 'markdown',
@@ -100,13 +127,101 @@ export default {
 
   create(context) {
     const { sourceCode } = context;
-    const [{ style }] = context.options;
+    const { lines } = sourceCode;
+    const [{ style, blankLineAbove, blankLineBelow }] = context.options;
 
     /** @type {CodeStyle | null} */
     let codeStyle = style === 'consistent' ? null : style;
 
     return {
       code(node) {
+        const {
+          start: { line: nodeStartLine },
+          end: { line: nodeEndLine },
+        } = sourceCode.getLoc(node);
+
+        // ------------------------------------------------------------------------
+        // 1. Check blank lines above the code block.
+        // ------------------------------------------------------------------------
+
+        if (blankLineAbove !== false) {
+          const nodeStartLineIndex = nodeStartLine - 1;
+
+          for (
+            let i = nodeStartLineIndex - 1; // Start checking from the line above the code block.
+            i >= nodeStartLineIndex - blankLineAbove; // Check up to the specified number of blank lines.
+            i-- // Move upwards through the lines.
+          ) {
+            const line = lines[i];
+
+            // If the line is `undefined`, it means we've reached the beginning of the file.
+            if (line === undefined) {
+              break;
+            }
+
+            // If the line is blank, continue checking the next line. If it's not blank, report the issue.
+            if (isBlankLine(line)) {
+              continue;
+            }
+
+            context.report({
+              node,
+
+              messageId: 'blankLineAbove',
+
+              data: {
+                blankLineAbove,
+              },
+            });
+
+            // No need to check further once we've found a non-blank line.
+            break;
+          }
+        }
+
+        // ------------------------------------------------------------------------
+        // 2. Check blank lines below the code block.
+        // ------------------------------------------------------------------------
+
+        if (blankLineBelow !== false) {
+          const nodeEndLineIndex = nodeEndLine - 1;
+
+          for (
+            let i = nodeEndLineIndex + 1; // Start checking from the line below the code block.
+            i <= nodeEndLineIndex + blankLineBelow; // Check up to the specified number of blank lines.
+            i++ // Move downwards through the lines.
+          ) {
+            const line = lines[i];
+
+            // If the line is `undefined`, it means we've reached the end of the file.
+            if (line === undefined) {
+              break;
+            }
+
+            // If the line is blank, continue checking the next line. If it's not blank, report the issue.
+            if (isBlankLine(line)) {
+              continue;
+            }
+
+            context.report({
+              node,
+
+              messageId: 'blankLineBelow',
+
+              data: {
+                blankLineBelow,
+              },
+            });
+
+            // No need to check further once we've found a non-blank line.
+            break;
+          }
+        }
+
+        // ------------------------------------------------------------------------
+        // 3. Check code style consistency.
+        // ------------------------------------------------------------------------
+
         const [nodeStartOffset] = sourceCode.getRange(node);
         const currentCodeStyle = getCurrentCodeStyle(sourceCode.text[nodeStartOffset]);
 
