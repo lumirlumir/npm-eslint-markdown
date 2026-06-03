8fc45bb5888de76c8fdf0d9bdbb6a54c9d7054b3
루밀LuMir <rpfos@naver.com>
2026-06-03 13:07:08 +0900

    wip

--- packages/eslint-markdown/src/core/ast/escape-string-regexp.js
+++ packages/eslint-markdown/src/core/ast/escape-string-regexp.js
@@ -48,7 +48,7 @@
  * new RegExp(escapedString);
  * ```
  */
-export default function escapeRegExp(string) {
+export default function escapeStringRegexp(string) {
   // Escape characters with special meaning either inside or outside character sets.
   // Use a simple backslash escape when it's always valid, and a `\xnn` escape
   // when the simpler form would be disallowed by Unicode patterns' stricter grammar.
