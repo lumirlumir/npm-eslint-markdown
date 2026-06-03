95ce9cef53681b95aa54fb2e29ea10ff6daf7143
루밀LuMir <rpfos@naver.com>
2026-06-03 15:54:19 +0900

    wip

--- packages/eslint-markdown/src/rules/require-heading-id.test.js
+++ packages/eslint-markdown/src/rules/require-heading-id.test.js
@@ -25,72 +25,172 @@ ruleTester('require-heading-id', rule, {
       name: 'Empty String',
       code: '  ',
     },
+
+    // Default: ATX Headings
     {
-      name: 'Correct h1 heading ID',
-      code: '# Heading {#id}',
+      name: 'ATX: Correct h1 heading ID',
+      code: '# Heading 1 {#heading-1}',
     },
     {
-      name: 'Correct h2 heading ID',
-      code: '## Heading {#id}',
+      name: 'ATX: Correct h2 heading ID',
+      code: '## Heading 2 {#heading-2}',
     },
     {
-      name: 'Correct h3 heading ID',
-      code: '### Heading {#id}',
+      name: 'ATX: Correct h3 heading ID',
+      code: '### Heading 3 {#heading-3}',
     },
     {
-      name: 'Correct h4 heading ID',
-      code: '#### Heading {#id}',
+      name: 'ATX: Correct h4 heading ID',
+      code: '#### Heading 4 {#heading-4}',
     },
     {
-      name: 'Correct h5 heading ID',
-      code: '##### Heading {#id}',
+      name: 'ATX: Correct h5 heading ID',
+      code: '##### Heading 5 {#heading-5}',
     },
     {
-      name: 'Correct h6 heading ID',
-      code: '###### Heading {#id}',
+      name: 'ATX: Correct h6 heading ID',
+      code: '###### Heading 6 {#heading-6}',
+    },
+
+    // Default: ATX Closed Headings
+    {
+      name: 'ATX Closed: Correct h1 heading ID',
+      code: '# Heading 1 {#heading-1} #',
+    },
+    {
+      name: 'ATX Closed: Correct h2 heading ID',
+      code: '## Heading 2 {#heading-2} ##',
+    },
+    {
+      name: 'ATX Closed: Correct h3 heading ID',
+      code: '### Heading 3 {#heading-3} ###',
+    },
+    {
+      name: 'ATX Closed: Correct h4 heading ID',
+      code: '#### Heading 4 {#heading-4} ####',
+    },
+    {
+      name: 'ATX Closed: Correct h5 heading ID',
+      code: '##### Heading 5 {#heading-5} #####',
+    },
+    {
+      name: 'ATX Closed: Correct h6 heading ID',
+      code: '###### Heading 6 {#heading-6} ######',
+    },
+
+    // Default: Setext Headings
+    {
+      name: 'Setext: Correct h1 heading ID',
+      code: 'Heading 1 {#heading-1}\n=========',
+    },
+    {
+      name: 'Setext: Correct multiline h1 heading ID',
+      code: 'Heading 1\nMultiple Lines {#heading-1}\n=========',
+    },
+    {
+      name: 'Setext: Correct h2 heading ID',
+      code: 'Heading 2 {#heading-2}\n---------',
+    },
+    {
+      name: 'Setext: Correct multiline h2 heading ID',
+      code: 'Heading 2\nMultiple Lines {#heading-2}\n---------',
     },
 
     // Default: Edge Cases
     {
-      name: 'h1 heading ID and trailing spaces',
+      name: 'ATX: h1 heading ID and trailing spaces',
       code: '# Heading {#id}  ',
     },
 
-    // `never` option
+    // `never` option: ATX Headings
     {
-      name: 'No h1 heading ID',
-      code: '# Heading',
+      name: 'ATX: No h1 heading ID',
+      code: '# Heading 1',
       options: ['never'],
     },
     {
-      name: 'No h2 heading ID',
-      code: '## Heading',
+      name: 'ATX: No h2 heading ID',
+      code: '## Heading 2',
       options: ['never'],
     },
     {
-      name: 'No h3 heading ID',
-      code: '### Heading',
+      name: 'ATX: No h3 heading ID',
+      code: '### Heading 3',
       options: ['never'],
     },
     {
-      name: 'No h4 heading ID',
-      code: '#### Heading',
+      name: 'ATX: No h4 heading ID',
+      code: '#### Heading 4',
       options: ['never'],
     },
     {
-      name: 'No h5 heading ID',
-      code: '##### Heading',
+      name: 'ATX: No h5 heading ID',
+      code: '##### Heading 5',
       options: ['never'],
     },
     {
-      name: 'No h6 heading ID',
-      code: '###### Heading',
+      name: 'ATX: No h6 heading ID',
+      code: '###### Heading 6',
+      options: ['never'],
+    },
+
+    // `never` option: ATX Closed Headings
+    {
+      name: 'ATX Closed: No h1 heading ID',
+      code: '# Heading 1 #',
+      options: ['never'],
+    },
+    {
+      name: 'ATX Closed: No h2 heading ID',
+      code: '## Heading 2 ##',
+      options: ['never'],
+    },
+    {
+      name: 'ATX Closed: No h3 heading ID',
+      code: '### Heading 3 ###',
+      options: ['never'],
+    },
+    {
+      name: 'ATX Closed: No h4 heading ID',
+      code: '#### Heading 4 ####',
+      options: ['never'],
+    },
+    {
+      name: 'ATX Closed: No h5 heading ID',
+      code: '##### Heading 5 #####',
+      options: ['never'],
+    },
+    {
+      name: 'ATX Closed: No h6 heading ID',
+      code: '###### Heading 6 ######',
+      options: ['never'],
+    },
+
+    // `never` option: Setext Headings
+    {
+      name: 'Setext: No h1 heading ID',
+      code: 'Heading 1\n=========',
+      options: ['never'],
+    },
+    {
+      name: 'Setext: No multiline h1 heading ID',
+      code: 'Heading 1\nMultiple Lines\n=========',
+      options: ['never'],
+    },
+    {
+      name: 'Setext: No h2 heading ID',
+      code: 'Heading 2\n---------',
+      options: ['never'],
+    },
+    {
+      name: 'Setext: No multiline h2 heading ID',
+      code: 'Heading 2\nMultiple Lines\n---------',
       options: ['never'],
     },
 
     // `leftDelimiter` and `rightDelimiter` option
     {
-      name: 'Custom Delimiters `[`, `]`',
+      name: 'ATX: Custom Delimiters `[`, `]`',
       code: '# Heading [#id]',
       options: [
         'always',
@@ -101,7 +201,7 @@ ruleTester('require-heading-id', rule, {
       ],
     },
     {
-      name: 'Custom Delimiters `|`, `|`',
+      name: 'ATX: Custom Delimiters `|`, `|`',
       code: '# Heading |#id|',
       options: [
         'always',
@@ -114,7 +214,7 @@ ruleTester('require-heading-id', rule, {
 
     // `allowDepths` option
     {
-      name: 'Ignore h1 heading ID',
+      name: 'ATX: Ignore h1 heading ID',
       code: '# Heading',
       options: [
         'always',
@@ -124,7 +224,7 @@ ruleTester('require-heading-id', rule, {
       ],
     },
     {
-      name: 'Ignore h1, h2, h3 heading ID',
+      name: 'ATX: Ignore h1, h2, h3 heading ID',
       code: '# Heading 1\n\n## Heading 2\n\n### Heading 3',
       options: [
         'always',
@@ -136,36 +236,10 @@ ruleTester('require-heading-id', rule, {
   ],
 
   invalid: [
-    // Default
+    // Default: ATX Headings
     {
-      name: 'Missing h1 heading ID',
-      code: '# Heading',
-      errors: [
-        {
-          messageId: 'headingIdAlways',
-          line: 1,
-          column: 10,
-          endLine: 1,
-          endColumn: 10,
-        },
-      ],
-    },
-    {
-      name: 'Missing h2 heading ID',
-      code: '## Heading',
-      errors: [
-        {
-          messageId: 'headingIdAlways',
-          line: 1,
-          column: 11,
-          endLine: 1,
-          endColumn: 11,
-        },
-      ],
-    },
-    {
-      name: 'Missing h3 heading ID',
-      code: '### Heading',
+      name: 'ATX: Missing h1 heading ID',
+      code: '# Heading 1',
       errors: [
         {
           messageId: 'headingIdAlways',
@@ -177,8 +251,8 @@ ruleTester('require-heading-id', rule, {
       ],
     },
     {
-      name: 'Missing h4 heading ID',
-      code: '#### Heading',
+      name: 'ATX: Missing h2 heading ID',
+      code: '## Heading 2',
       errors: [
         {
           messageId: 'headingIdAlways',
@@ -190,8 +264,8 @@ ruleTester('require-heading-id', rule, {
       ],
     },
     {
-      name: 'Missing h5 heading ID',
-      code: '##### Heading',
+      name: 'ATX: Missing h3 heading ID',
+      code: '### Heading 3',
       errors: [
         {
           messageId: 'headingIdAlways',
@@ -203,8 +277,8 @@ ruleTester('require-heading-id', rule, {
       ],
     },
     {
-      name: 'Missing h6 heading ID',
-      code: '###### Heading',
+      name: 'ATX: Missing h4 heading ID',
+      code: '#### Heading 4',
       errors: [
         {
           messageId: 'headingIdAlways',
@@ -215,10 +289,36 @@ ruleTester('require-heading-id', rule, {
         },
       ],
     },
+    {
+      name: 'ATX: Missing h5 heading ID',
+      code: '##### Heading 5',
+      errors: [
+        {
+          messageId: 'headingIdAlways',
+          line: 1,
+          column: 16,
+          endLine: 1,
+          endColumn: 16,
+        },
+      ],
+    },
+    {
+      name: 'ATX: Missing h6 heading ID',
+      code: '###### Heading 6',
+      errors: [
+        {
+          messageId: 'headingIdAlways',
+          line: 1,
+          column: 17,
+          endLine: 1,
+          endColumn: 17,
+        },
+      ],
+    },
 
     // Default: Edge Cases
     {
-      name: 'h1 heading ID with nothing',
+      name: 'ATX: h1 heading ID with nothing',
       code: '# Heading {#}',
       errors: [
         {
@@ -231,7 +331,7 @@ ruleTester('require-heading-id', rule, {
       ],
     },
     {
-      name: 'h1 heading ID with leading space',
+      name: 'ATX: h1 heading ID with leading space',
       code: '# Heading { #id}',
       errors: [
         {
@@ -244,9 +344,9 @@ ruleTester('require-heading-id', rule, {
       ],
     },
 
-    // Never option
+    // `never` option: ATX Headings
     {
-      name: 'h1 heading ID exists',
+      name: 'ATX: h1 heading ID exists',
       code: '# Heading {#id}',
       output: '# Heading ',
       errors: [
@@ -261,7 +361,7 @@ ruleTester('require-heading-id', rule, {
       options: ['never'],
     },
     {
-      name: 'h2 heading ID exists',
+      name: 'ATX: h2 heading ID exists',
       code: '## Heading {#id}',
       output: '## Heading ',
       errors: [
@@ -276,7 +376,7 @@ ruleTester('require-heading-id', rule, {
       options: ['never'],
     },
     {
-      name: 'h3 heading ID exists',
+      name: 'ATX: h3 heading ID exists',
       code: '### Heading {#id}',
       output: '### Heading ',
       errors: [
@@ -291,7 +391,7 @@ ruleTester('require-heading-id', rule, {
       options: ['never'],
     },
     {
-      name: 'h4 heading ID exists',
+      name: 'ATX: h4 heading ID exists',
       code: '#### Heading {#id}',
       output: '#### Heading ',
       errors: [
@@ -306,7 +406,7 @@ ruleTester('require-heading-id', rule, {
       options: ['never'],
     },
     {
-      name: 'h5 heading ID exists',
+      name: 'ATX: h5 heading ID exists',
       code: '##### Heading {#id}',
       output: '##### Heading ',
       errors: [
@@ -321,7 +421,7 @@ ruleTester('require-heading-id', rule, {
       options: ['never'],
     },
     {
-      name: 'h6 heading ID exists',
+      name: 'ATX: h6 heading ID exists',
       code: '###### Heading {#id}',
       output: '###### Heading ',
       errors: [
